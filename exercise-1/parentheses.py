a298a5dc822bea59a2bd49e816b00824204731be
Thanos Paravantis <p16112@students.cs.unipi.gr>
2017-12-30 16:25:12 +0200

    First completed version of ex-1

--- exercise-1/parentheses.py
+++ exercise-1/parentheses.py
@@ -25,9 +25,53 @@ def main(args):
     # Format input
 
     contents = formatInput(contents)
-    print("Contents: " + contents)
 
-    # ...
+    # Setup push-down automaton
+    # K = { k0, k1, k2 }
+    # T = { (, ) }
+    # V = { (, ), $ }
+    # k1 = k0
+    # A1 = $
+    # F = { t2 }
+
+    # f(k0, e, e) = { (k1, $) }
+    # f(k1, e, '(') = { (k1, '(') }
+    # f(k1, '(', ')') = { (k1, e) }
+    # f(k1, $, e) = { (k2, e) }
+
+    stack = ["$"]
+    state = "k1"
+
+    while True:
+        char = contents[0] if len(contents) > 0 else ""
+        print("Now: " + char)
+        if char == "(":
+            contents = contents[1:]
+            print("Contents: " + contents)
+            stack.append(char)
+            state = "k1"
+            print("PUSH: " + str(stack))
+        elif stack[-1] == "(" and char == ")":
+            contents = contents[1:]
+            print("Contents: " + contents)
+            stack.pop()
+            state = "k1"
+            print("POP: " + str(stack))
+        elif state == "k1" and stack[-1] == "$" and char == "":
+            contents = contents[1:]
+            print("Contents: " + contents)
+            state = "k2"
+            print("END: " + str(stack))
+            break
+        else:
+            print("Contents: " + contents)
+            print("BREAK: " + str(stack))
+            break
+
+    if stack[-1] == "$" and state == "k2":
+        print("YES")
+    else:
+        print("NO")
 
 
 def formatInput(string):
